fabbce5531205d999243a0e969cb4782afdeb555
Thomas <thomasfindlay94@gmail.com>
2022-03-19 19:11:19 +0000

    wip

--- src/components/BlogPosts.jsx
+++ src/components/BlogPosts.jsx
@@ -1,16 +1,11 @@
-import { useState, useEffect } from "react";
 import {
   GridLayout,
   GridLayoutItem,
   StackLayout,
 } from "@progress/kendo-react-layout";
-import useBreakpoint from "use-breakpoint";
+import { useState, useEffect } from "react";
 import styles from "./BlogPosts.module.css";
-import { GRID_CONFIG } from "../constants/gridConfig";
-import { BREAKPOINTS } from "../constants/breakpoints";
-
 const BlogPosts = props => {
-  const { breakpoint } = useBreakpoint(BREAKPOINTS, "desktop");
   const [posts, setPosts] = useState([]);
   const [featuredPosts, setFeaturedPosts] = useState([]);
 
@@ -29,70 +24,63 @@ const BlogPosts = props => {
 
   return (
     <div>
+      <h1>Welcome to my blog</h1>
       <div>
-        <h1>Welcome to my blog</h1>
-        <div>
-          <GridLayout
-            gap={{
-              rows: 20,
-              cols: 40,
-            }}
-            rows={GRID_CONFIG[breakpoint].outerGrid.rows}
-            cols={GRID_CONFIG[breakpoint].outerGrid.cols}
-          >
-            <GridLayoutItem
-              row={GRID_CONFIG[breakpoint].postsContainer.row}
-              col={GRID_CONFIG[breakpoint].postsContainer.col}
+        <GridLayout
+          gap={{
+            rows: 20,
+            cols: 40,
+          }}
+          cols={[
+            {
+              width: "70%",
+            },
+            {
+              width: "30%",
+            },
+          ]}
+        >
+          <GridLayoutItem row={1} col={1}>
+            <h2>Recent posts</h2>
+            <GridLayout
+              gap={{ rows: 20, cols: 20 }}
+              cols={[
+                {
+                  width: "repeat(3, 1fr)",
+                },
+              ]}
             >
-              <h2>Recent posts</h2>
-              <GridLayout
-                gap={{ rows: 20, cols: 20 }}
-                cols={GRID_CONFIG[breakpoint].posts.cols}
-              >
-                {posts.map((post, idx) => {
-                  const row =
-                    Math.floor(idx / GRID_CONFIG[breakpoint].postItem.divider) +
-                    1;
-                  return (
-                    <GridLayoutItem
-                      className={styles.postCard}
-                      key={post.id}
-                      row={row}
-                      col={(idx % GRID_CONFIG[breakpoint].postItem.divider) + 1}
-                    >
-                      <h3 className={styles.postHeading}>{post.title}</h3>
-                      <p className={styles.postBody}>{post.body}</p>
-                    </GridLayoutItem>
-                  );
-                })}
-              </GridLayout>
-            </GridLayoutItem>
-            <GridLayoutItem
-              row={GRID_CONFIG[breakpoint].featuredContainer.row}
-              col={GRID_CONFIG[breakpoint].featuredContainer.col}
-            >
-              <h2>Featured posts</h2>
-              <StackLayout
-                orientation={GRID_CONFIG[breakpoint].featuredOrientation}
-                gap={20}
-              >
-                {(breakpoint === "desktop"
-                  ? featuredPosts
-                  : featuredPosts.slice(0, 3)
-                ).map(featuredPost => {
-                  return (
-                    <div className={styles.postCard} key={featuredPost.id}>
-                      <h3 className={styles.postHeading}>
-                        {featuredPost.title}
-                      </h3>
-                      <p className={styles.postBody}>{featuredPost.body}</p>
-                    </div>
-                  );
-                })}
-              </StackLayout>
-            </GridLayoutItem>
-          </GridLayout>
-        </div>
+              {posts.map((post, idx) => {
+                const row = Math.floor(idx / 3) + 1;
+                const col = (idx % 3) + 1;
+                return (
+                  <GridLayoutItem
+                    className={styles.postCard}
+                    key={post.id}
+                    row={row}
+                    col={col}
+                  >
+                    <h3 className={styles.postHeading}>{post.title}</h3>
+                    <p className={styles.postBody}>{post.body}</p>
+                  </GridLayoutItem>
+                );
+              })}
+            </GridLayout>
+          </GridLayoutItem>
+          <GridLayoutItem row={1} col={2}>
+            <h2>Featured posts</h2>
+            <StackLayout orientation="vertical" gap={20}>
+              {featuredPosts.map(featuredPost => {
+                return (
+                  <div className={styles.postCard} key={featuredPost.id}>
+                    <h3 className={styles.postHeading}>{featuredPost.title}</h3>
+                    <p className={styles.postBody}>{featuredPost.body}</p>
+                  </div>
+                );
+              })}
+            </StackLayout>
+          </GridLayoutItem>
+        </GridLayout>
       </div>
     </div>
   );
